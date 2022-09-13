f0112241ca62a01a24baa1306b86e29d1049c509
twyle <lyceokoth@gmail.com>
2022-09-13 10:13:21 +0300

    prinst lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase, followed by a new line
+ *
+ * Return: Nothing
+ */
+void print_alphabet(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+	char c;
+	int i = 0;
+	while (i < 26)
+	{
+		c = alphabet[i];
+		_putchar(c);
+		i++;
+	}
+	_putchar('\n');
+	return; 
+}
