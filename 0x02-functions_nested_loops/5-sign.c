83e99b51a3b46cc77ffa72b3d5bfd10f1f2dad55
twyle <lyceokoth@gmail.com>
2022-09-13 11:40:11 +0300

    prints the sign of an int

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: the number to check
+ * Return: 0 or 1 or 2
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
