524351e05ad18c291e8b7faaee054477c7906708
twyle <lyceokoth@gmail.com>
2022-09-14 08:31:22 +0300

    fixed the betty errors

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 
 /**
- * main - prints the first 50 fibonacci numbers
+ * main - Prints the first 50 fibonacci numbers
  *
- * Return: 0 Alwsys (Success)
+ * Return: 0 Always (success)
  */
 int main(void)
 {
-        unsigned long int res;
-        unsigned long int n1 = 0;
-        unsigned long int n2 = 1;
-        int count;
+	unsigned long int res;
+	unsigned long int n1 = 0;
+	unsigned long int n2 = 1;
+	int count;
 
-        for (count = 1; count <= 50; count++)
-        {
-                res = n1 + n2;
+	for (count = 1; count <= 50; count++)
+	{
+		res = n2 + n1;
 		if (count != 50)
 			printf("%ld, ", res);
 		else
 			printf("%ld\n", res);
-                n1 = n2;
-                n2 = res;
-        }
-        return (0);
+		n1 = n2;
+		n2 = res;
+	}
+	return (0);
 }
