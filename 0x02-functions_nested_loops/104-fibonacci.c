a10dfc025d74ef98e01d3d4a95d9fe0b2da432d4
twyle <lyceokoth@gmail.com>
2022-09-14 09:45:47 +0300

    prints first 98 fib no

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,24 +1,45 @@
 #include <stdio.h>
-
 /**
- * main - prints the first 98 fibonacci numbers
- *
- * Return: 0 Alwsys (Success)
- */
+  * main - print the first 98 fibonacci numbers.
+  * Return: Nothing.
+  */
 int main(void)
 {
-	unsigned long int res;
-	unsigned long int n1 = 0;
-	unsigned long int n2 = 1;
 	int count;
+	unsigned long i, j, k;
+	unsigned long m, n, p, carry;
 
-	for (count = 1; count <= 98; count++)
+	count = 0;
+	i = 0;
+	j = 1;
+	for (count = 1; count <= 91; count++)
 	{
-		res = n1 + n2;
-		printf("%ld, ", res);
-		n1 = n2;
-		n2 = res;
+		k = i + j;
+		i = j;
+		j = k;
+		printf("%lu, ", k);
 	}
-	printf("\n");
+	m = i % 1000;
+	i = i / 1000;
+	n = j % 1000;
+	j = j / 1000;
+	while (count <= 98)
+	{
+		carry = (m + n) / 1000;
+		p = (m + n) - carry * 1000;
+		k = (i + j) + carry;
+		m = n;
+		n = p;
+		i = j;
+		j = k;
+		if (p >= 100)
+			printf("%lu%lu", k, p);
+		else
+			printf("%lu0%lu", k, p);
+		if (count != 98)
+			printf(", ");
+		count++;
+	}
+	putchar('\n');
 	return (0);
 }
