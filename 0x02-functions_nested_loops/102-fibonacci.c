a28ae67ec28c4fa0c9628dd55bb2bb6526afaa78
twyle <lyceokoth@gmail.com>
2022-09-14 08:12:06 +0300

    prints the first 50 fibonnacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 fibonacci numbers
+ *
+ * Return: 0 Alwsys (Success)
+ */
+int main(void)
+{
+        unsigned long int res;
+        unsigned long int n1 = 0;
+        unsigned long int n2 = 1;
+        int count;
+
+        for (count = 1; count <= 50; count++)
+        {
+                res = n1 + n2;
+                printf("%ld, ",res);
+                n1 = n2;
+                n2 = res;
+        }
+        printf("\n");
+        return (0);
+}
