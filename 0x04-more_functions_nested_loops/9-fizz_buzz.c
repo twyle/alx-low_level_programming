0cd986b8b279eb815ffc7627d9643655f15cfbc7
twyle <lyceokoth@gmail.com>
2022-09-15 17:45:02 +0300

    fizbuzz 1-100

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Fizzbuzz for 3 and 5
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int count;
+
+	for (count = 1; count <= 100; count++)
+	{
+		if (count % 3 == 0 && count % 5 == 0)
+			printf("FizzBuzz ");
+		else if (count % 3 == 0)
+			printf("Fizz ");
+		else if (count % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", count);
+	}
+	printf("\n");
+	return (0);
+}
