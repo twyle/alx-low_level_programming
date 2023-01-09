130de4ced24aff23b38e8d6fc4576ffeb653ffa9
twyle <lyceokoth@gmail.com>
2023-01-09 18:04:59 +0300

    initial commit

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,12 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/*
+ * File: holberton.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file containing the prototypes of all functions
+ *       included in the static library libholberton.a.
+ */
 
 int _putchar(char c);
 int _islower(int c);
@@ -22,4 +29,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* _MAIN_H_ */
+#endif
